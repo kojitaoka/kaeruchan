e87f2c7e565bc8a770857ca19727d69b4e34218b
taoka3331 <taokamasaharu@gmail.com>
2018-08-11 04:50:47 +0000

    disc.song mske colum

--- db/migrate/20180811033127_create_discs.rb
+++ db/migrate/20180811033127_create_discs.rb
@@ -3,6 +3,11 @@ class CreateDiscs < ActiveRecord::Migration[5.2]
     create_table :discs do |t|
 
       t.timestamps
+      t.integer :disc_id
+      t.integer :item_id
+      t.string :disc_number
+      t.datetime :create_at
+      t.datetime :update_at
     end
   end
 end
