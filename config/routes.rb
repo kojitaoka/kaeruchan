b35f5554930ec8206df851849f574a140c0218d9
kozy10 <kentaro1002vb@gmail.com>
2018-08-11 02:40:27 +0000

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+	devise_for :admins, path: 'admin/admins', controllers: {
+	  sessions:      'admin/admins/sessions',
+	  passwords:     'admin/admins/passwords',
+	  registrations: 'admin/admins/registrations'
+	}
+	devise_for :users, controllers: {
+	  sessions:      'user/users/sessions',
+	  passwords:     'user/users/passwords',
+	  registrations: 'user/users/registrations'
+	}
+
+	namespace :admin, path: 'admin' do
+		root 'home#index', as: :root
+	end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
