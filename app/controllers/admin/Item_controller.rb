2888ab17b0166508c5e342435b14efc004133b50
kozy10 <kentaro1002vb@gmail.com>
2018-08-11 06:38:48 +0000

    ok

--- app/controllers/admin/Item_controller.rb
+++ app/controllers/admin/Item_controller.rb
@@ -3,4 +3,20 @@ class Admin::ItemController < ApplicationController
 		render 'admin/home'
 	end
 
+	def new
+		@item = Item.new
+		@item.discs.build
+		@item.songs.build
+	end
+
+	def create
+		item = Item.new(item_params)
+		item.save
+	end
+
+	private
+
+		def item_params
+			params.require(:item).permit()
+
 end
